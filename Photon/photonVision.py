6390cf7bd3e0b4bd8e888b822bc058a4aaeff0c1
Mr. Roth-Ritchie <jrothritchie@hdsd.org>
2021-08-08 18:49:31 -0400

    Update photonVision.py

--- Photon/photonVision.py
+++ Photon/photonVision.py
@@ -3,7 +3,7 @@ import os
 
 
 try:
-    os.system("java -jar photonvision.jar")
+    os.system("java -jar photonvision.jar") #change photovision.jar to match the filename of your photon jar file.
     print ("Photon Vision has started. Visit wpilibpi.local:5800 (or your Romi's ip address:5800) to tune your pipelines.")
 except:
     print("Unexpected error:", sys.exc_info()[0])
